cb1c00284ba2668af2789adc7a5c0c79e00dc11f
Ziad Assem <ziadassem3000@gmail.com>
2025-05-28 01:39:22 +0300

    commented outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,15 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.main.repository_url
-}
+# output "ecr_repository_url" {
+#   value = aws_ecr_repository.main.repository_url
+# }
 
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.main.name
-}
+# output "ecs_cluster_name" {
+#   value = aws_ecs_cluster.main.name
+# }
 
-output "ecs_service_name" {
-  value = aws_ecs_service.main.name
-}
+# output "ecs_service_name" {
+#   value = aws_ecs_service.main.name
+# }
 
-output "task_execution_role_arn" {
-  value = aws_iam_role.ecsTaskExecutionRole.arn
-}
+# output "task_execution_role_arn" {
+#   value = aws_iam_role.ecsTaskExecutionRole.arn
+# }
